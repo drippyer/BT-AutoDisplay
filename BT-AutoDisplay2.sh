5f87a76decdb1064f9fb33bcd1d7ecb5b4b5f1e8
drippyer <parker.capron@gmail.com>
2018-10-27 15:23:16 -0500

    Update BT-AutoDisplay2.sh
    added display_on and display_off functions to make it simpler to run/modify in the future

--- BT-AutoDisplay2.sh
+++ BT-AutoDisplay2.sh
@@ -11,16 +11,28 @@
 #                                                     #
 #######################################################
 
-MAC="ENTER MAC ADDRESS HERE"
+MAC="ENTER BLUETOOTH MAC ADDRESS HERE"
 output="$(python bluetooth-proximity/examples/test_address.py $MAC)"
 status=""
 
+display_on()
+{
+	vcgencmd display_power 1
+	echo "on" >| /tmp/display_status.dat
+	status="on"
+}
+
+display_off()
+{
+	vcgencmd display_power 0
+	echo "off" >| /tmp/display_status.dat
+	status="off"
+}
+
 # if /tmp/display_status.dat does not exist, set status to "on" then create .dat
 if [ ! -f "/tmp/display_status.dat" ]; then
-	echo "status file NOT found"
-	vcgencmd display_power 1
-	status="on" 
-	echo $status >| /tmp/display_status.dat
+	echo "status file does not exist, creating one and turning display on..."
+	display_on
 # otherwise set status to the value in /tmp/display_status.dat ("on"/"off")
 else
 	status=`cat /tmp/display_status.dat`
@@ -32,9 +44,7 @@ if [ $output = "None" ]; then
 	# ... and display was on before, then turn display off
 	if [ $status = "on" ]; then
 		echo "bluetooth has disconnected, turning display off..."
-		vcgencmd display_power 0
-		status="off" 
-		echo $status >| /tmp/display_status.dat
+		display_off
 	# ... and display was off before, then do nothing
 	else
 		echo "bluetooth not connected but display already off, not doing anything"
@@ -44,9 +54,7 @@ else
 	# ... and display was off before, then turn the display on
 	if [ $status = "off" ]; then
 		echo "bluetooth reconnected, turning display on..."
-		vcgencmd display_power 1
-		status="on" 
-		echo $status >| /tmp/display_status.dat
+		display_on
 	else
 		echo "bluetooth connected but display already on, not doing anything"
 	fi
